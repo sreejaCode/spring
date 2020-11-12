fb76b9b01df36995ce01787899a196e1feb214bc
Sreeja Menon <sreejam@gmail.com>
2020-11-12 15:48:23 +0100

    added the comments

---
+++ src/main/java/se/sda/web/springproject/comments/CommentsService.java
@@ -0,0 +1,49 @@
+package se.sda.web.springproject.comments;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
+public class CommentsService {
+    @Autowired
+    private CommentsRepository repository;
+
+    public List<Comments> getAll() {
+        return repository.findAll();
+    }
+
+
+    public Optional<Comments> getById(Long id) {
+        return repository.findById(id);
+
+    }
+
+    public Comments create(Comments comment) {
+        return repository.save(comment);
+
+    }
+
+    public void delete(Long id) {
+        repository.deleteById(id);
+
+    }
+
+    public Comments update(Comments updatedcomment) {
+        return repository.save(updatedcomment);
+
+    }
+
+
+
+
+
+}
+
+
+
+
+
